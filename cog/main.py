11a8ee3237092a868c0e69a9e3188c0eb8a9f0df
Jack Greisman <Jack.Greisman@gmail.com>
2019-11-14 15:01:13 -0500

    Fix commandline arguments parsed in cog/main.py

--- cog/main.py
+++ cog/main.py
@@ -13,15 +13,18 @@ def main():
     parser_softlimits = subparsers.add_parser("softlimits",
                                               help="Determine soft limits for data analysis")
     parser_softlimits.add_argument("-i", "--image", help="Image to use for determining soft limits")
-    parser_softlimits.add_argument("-c", "--cell", help="Cell parameters for crystal (a, b, c, alpha, beta, gamma)",
+    parser_softlimits.add_argument("-c", "--cell", help="Cell parameters for crystal",
+                                   metavar=("a", "b", "c", "alpha", "beta", "gamma"),
                                    nargs=6, type=float)
     parser_softlimits.add_argument("-d", "--distance", help="Detector distance in mm",
                                    type=float)
     parser_softlimits.add_argument("--center", help="Coordinates of beam center in pixels",
+                                   metavar=("center_x", "center_y"),
                                    nargs=2, type=float)
     parser_softlimits.add_argument("-r", "--resolution", help="High-resolution limit in angstroms",
                                    type=float)
-    parser_softlimits.add_argument("-s", "--spot_profile", help="Parameters to be used for spot recognition (length, width, sigma-cut)",
+    parser_softlimits.add_argument("-s", "--spot_profile", help="Parameters to be used for spot recognition",
+                                   metavar=("length", "width", "sigma_cut"),
                                    nargs=3, type=float)
     parser_softlimits.add_argument("-o", "--outfile", help="File to which spot locations should be written")
     parser_softlimits.set_defaults(func=softlimits)
