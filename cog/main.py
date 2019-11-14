544c2f1175f2ba7be5360aff06206c86e9b36e78
Jack Greisman <Jack.Greisman@gmail.com>
2019-11-14 14:54:48 -0500

    Update call signature of softlimits.softlimit()

--- cog/main.py
+++ cog/main.py
@@ -12,7 +12,18 @@ def main():
     # Parser for softlimits
     parser_softlimits = subparsers.add_parser("softlimits",
                                               help="Determine soft limits for data analysis")
-    parser_softlimits.add_argument("-i", "--image", help="image to use for determining soft limits")
+    parser_softlimits.add_argument("-i", "--image", help="Image to use for determining soft limits")
+    parser_softlimits.add_argument("-c", "--cell", help="Cell parameters for crystal (a, b, c, alpha, beta, gamma)",
+                                   nargs=6, type=float)
+    parser_softlimits.add_argument("-d", "--distance", help="Detector distance in mm",
+                                   type=float)
+    parser_softlimits.add_argument("--center", help="Coordinates of beam center in pixels",
+                                   nargs=2, type=float)
+    parser_softlimits.add_argument("-r", "--resolution", help="High-resolution limit in angstroms",
+                                   type=float)
+    parser_softlimits.add_argument("-s", "--spot_profile", help="Parameters to be used for spot recognition (length, width, sigma-cut)",
+                                   nargs=3, type=float)
+    parser_softlimits.add_argument("-o", "--outfile", help="File to which spot locations should be written")
     parser_softlimits.set_defaults(func=softlimits)
     
     # Parse commandline arguments and call subcommand
--- cog/softlimits/softlimits.py
+++ cog/softlimits/softlimits.py
@@ -1,7 +1,32 @@
-def softlimits(image=None):
+def softlimits(image=None, cell=None, distance=None, center=None,
+               resolution=2.0, spot_profile=(10, 5, 2.0), outfile="spots.spt"):
     """
     Determine soft limits for data analsysis using Precognition's spot 
     profile and resolution estimates
+
+    Parameters
+    ----------
+    image : filename
+        Path to image to be analyzed. For now, it is assumed to be a
+        MCCD image from a RayonixMX340 detector
+    cell : tuple(a, b, c, alpha, beta, gamma)
+        Tuple of cell parameters for crystal
+    distance : float
+        Detector distance in mm
+    center : tuple(center_x, center_y)
+        Coordinates of beam center in pixels
+    resolution : float
+        High-resolution limit in angstroms
+    spot_profile : tuple(length, width, sigma-cut)
+        Parameters to be used for spot recognition
+    outfile : filename
+        File to which spot locations should be written
     """
     print(image)
+    print(cell)
+    print(distance)
+    print(center)
+    print(resolution)
+    print(spot_profile)
+    print(outfile)
     return
