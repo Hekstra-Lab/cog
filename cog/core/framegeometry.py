873bac371a3896a1c5174d8865369595855cfb8f
Jack Greisman <Jack.Greisman@gmail.com>
2020-01-17 11:23:00 -0500

    Add template for FrameGeometry class for storing experimental geometry

---
+++ cog/core/framegeometry.py
@@ -0,0 +1,30 @@
+class FrameGeometry():
+    """
+    Provides a representation of  per-frame geometry in Precognition.
+
+    Includes attributes for accessing geometric parameters, and methods
+    for reading and writing Precognition .inp geometry files. 
+    """
+
+    def __init__(self, inpfile):
+        pass
+
+    def readINPFile(self, inpfile):
+        """
+        Read Precognition .inp file and update geometric attributes
+
+        Parameters
+        ----------
+        inpfile : str
+            Path to .inp file from which to read
+        """
+
+    def writeINPFile(self, inpfile):
+        """
+        Write Precognition .inp file containing experimental geometry
+
+        Parameters
+        ----------
+        inpfile : str
+            Path to .inp file to which to write
+        """
