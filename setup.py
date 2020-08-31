9d62919c866e25c1f1fcad1976aca26b79466e12
Jack Greisman <Jack.Greisman@gmail.com>
2020-08-31 11:11:40 -0400

    Bump version to 0.3.1 due to added cog.facet method

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cog',
-    version='0.3.0',
+    version='0.3.1',
     author='Jack B. Greisman',
     author_email='greisman@g.harvard.edu',
     packages=find_packages(),
