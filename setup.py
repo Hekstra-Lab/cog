c705ed35498c55908bf56afcbbec7d8fc891de0c
Jack Greisman <Jack.Greisman@gmail.com>
2021-09-15 15:15:22 -0400

    Bump version to 0.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cog",
-    version="0.3.1",
+    version="0.3.2",
     author="Jack B. Greisman",
     author_email="greisman@g.harvard.edu",
     packages=find_packages(),
