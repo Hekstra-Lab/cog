aef2249d7fab545e1ecc32da40652848a8ed73f5
dennisbrookner <debrookner@gmail.com>
2022-08-17 15:00:43 -0400

    initial commit of new branch

--- tests/core/test_experiment.py
+++ tests/core/test_experiment.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 from cog import Experiment
 
+
 def test_constructor_minimal():
     """Test minimal Experiment constructor"""
     ds = Experiment(pd.DataFrame(), "./")
@@ -18,41 +19,29 @@ def test_constructor_minimal():
 @pytest.mark.parametrize("images", [pd.DataFrame()])
 @pytest.mark.parametrize("pathToImages", ["./"])
 @pytest.mark.parametrize("distance", [None, 100, 100.0])
-@pytest.mark.parametrize("center", [
-    None,
-    (1990, 1970),
-    [1990,  1970],
-    (1991., 1970.),
-    [1990., 1970.]
-])
-@pytest.mark.parametrize("pixelSize", [
-    None,
-    (88, 88),
-    [88,  88],
-    (88.6, 88.6),
-    [88.6, 88.6]
-])
-@pytest.mark.parametrize("cell", [
-    None,
-    [1., 2., 3., 90., 90., 90.],
-    (1., 2., 3., 90., 90., 90.)
-])
-@pytest.mark.parametrize("spacegroup", [
-    None,
-    1,
-    19,
-    96,
-    1.0
-])
-def test_constructor(images, pathToImages, distance, center, pixelSize, cell, spacegroup):
+@pytest.mark.parametrize(
+    "center", [None, (1990, 1970), [1990, 1970], (1991.0, 1970.0), [1990.0, 1970.0]]
+)
+@pytest.mark.parametrize(
+    "pixelSize", [None, (88, 88), [88, 88], (88.6, 88.6), [88.6, 88.6]]
+)
+@pytest.mark.parametrize(
+    "cell", [None, [1.0, 2.0, 3.0, 90.0, 90.0, 90.0], (1.0, 2.0, 3.0, 90.0, 90.0, 90.0)]
+)
+@pytest.mark.parametrize("spacegroup", [None, 1, 19, 96, 1.0])
+def test_constructor(
+    images, pathToImages, distance, center, pixelSize, cell, spacegroup
+):
     """Test Experiment constructor"""
-    ds = Experiment(images,
-                    pathToImages,
-                    distance=distance,
-                    center=center,
-                    pixelSize=pixelSize,
-                    cell=cell,
-                    spacegroup=spacegroup)
+    ds = Experiment(
+        images,
+        pathToImages,
+        distance=distance,
+        center=center,
+        pixelSize=pixelSize,
+        cell=cell,
+        spacegroup=spacegroup,
+    )
 
     if distance is None:
         assert ds.distance is None
@@ -72,7 +61,7 @@ def test_constructor(images, pathToImages, distance, center, pixelSize, cell, sp
         assert ds.pixelSize == (float(pixelSize[0]), float(pixelSize[1]))
 
     if cell is None:
-        assert ds.cell == tuple([None]*6)
+        assert ds.cell == tuple([None] * 6)
     else:
         assert ds.cell == (cell[0], cell[1], cell[2], cell[3], cell[4], cell[5])
 
@@ -80,4 +69,3 @@ def test_constructor(images, pathToImages, distance, center, pixelSize, cell, sp
         assert ds.spacegroup is None
     else:
         assert ds.spacegroup == int(spacegroup)
-
