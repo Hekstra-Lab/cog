e5babfa89f52dee242c35764b23acb11e2c134e2
hkwang <98353449+hkwang@users.noreply.github.com>
2023-06-15 13:53:34 -0400

    precognition environment on the new cluster OS
    During the Rocky 8 cluster operating system change, many modules, including gcc 7.1.0 (necessary for compiling Fortran code), were removed. The present change utilizes a spack installation of gcc 7.1.0.

--- cog/core/precognition.py
+++ cog/core/precognition.py
@@ -14,11 +14,12 @@ def run(inpfile, logfile):
         File to which Precognition log will be written
     """
     # Paths
+    spack_source = "/n/holylfs05/LABS/hekstra_lab/Lab/garden/lib/spack/share/spack/setup-env.sh"
     garden = "/n/holylfs05/LABS/hekstra_lab/Lab/garden"
     precognition = f"{garden}/precognition/Precognition_5.2_distrib"
 
     # Commands
-    setenv = f"source {precognition}/setup_precognition_env.sh"
+    setenv = f"source {spack_source}; spack load gcc; source {precognition}/setup_precog_spack.sh"
     precog = f"Precognition_T5.2.2_x86_64 {inpfile} > {logfile}"
     cmd = f"{setenv}; {precog}"
 
